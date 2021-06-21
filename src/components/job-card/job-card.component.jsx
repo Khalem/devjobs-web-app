9a4f5d19db64ffdd808eedcc70c37b8568d6640c
Khalem <33363455+Khalem@users.noreply.github.com>
2021-06-21 15:36:03 +0100

    finished JobCard component

--- src/components/job-card/job-card.component.jsx
+++ src/components/job-card/job-card.component.jsx
@@ -2,16 +2,24 @@ import { getLogoUrl } from '../../utils/logo.utils';
 
 import './job-card.styles.scss';
 
-const JobCard = ({ obj: { logo, logoBackground, company } }) => {
+const JobCard = ({ obj: { logo, logoBackground, company, postedAt, contract, position, location } }) => {
     return (
         <div className='job-card'>
             <div className='logo-wrapper' style={{ backgroundColor: logoBackground }} >
-            <img 
-                src={getLogoUrl(logo)} 
-                className='logo'
-                alt={company} 
-            />
+                <img 
+                    src={getLogoUrl(logo)} 
+                    className='logo'
+                    alt={company} 
+                />
             </div>
+            <div className='time-posted-fulltime-wrapper'>
+                <p>{postedAt}</p>
+                <div className='dot' />
+                <p>{contract}</p>
+            </div>
+            <h3 className='job-title'>{position}</h3>
+            <p className='job-company'>{company}</p>
+            <h4 className='job-location'>{location}</h4>
         </div>
     );
 };
