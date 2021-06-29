cd65e84a268a0d3b53461c31f5af6828d97e3dc5
Khalem <33363455+Khalem@users.noreply.github.com>
2021-06-29 12:07:42 +0100

    added darkmode to app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import ThemeSlider from '../theme-slider/theme-slider.component';
 
+import { ReactComponent as Logo } from '../../assets/desktop/logo.svg';
+
 import './header.styles.scss';
 
 const Header = () => (
     <header className='header'>
         <div className='header-content'>
-            <h1 className='header-title'>devjobs</h1>
+            <Logo />
             <ThemeSlider />
         </div>
     </header>
--- src/components/theme-slider/theme-slider.component.jsx
+++ src/components/theme-slider/theme-slider.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'; 
+import { useState, useEffect } from 'react'; 
 
 import { ReactComponent as LightIcon } from '../../assets/desktop/icon-sun.svg';
 import { ReactComponent as DarkIcon } from '../../assets/desktop/icon-moon.svg';
@@ -8,7 +8,36 @@ import './theme-slider.styles.scss';
 const ThemeSlider = () => {
     const [toggled, setToggle] = useState(false);
 
-    const mode = toggled ? 'dark' : 'light'; 
+    const mode = toggled ? 'dark' : 'light';
+
+    const themes = {
+        light: {
+            '--violet-button': '#5964E0',
+            '--dark-blue-text': '#19202D',
+            '--white': '#ffffff',
+            '--light-grey': '#F4F6F8',
+            '--blue-opacity': 'rgba(89, 100, 224, .1)',
+            '--blue-opacity-hover': 'rgba(89, 100, 224, .35)',
+            '--checkbox-unchecked': 'rgba(25, 32, 45, .135)'
+        },
+        dark: {
+            '--white': '#19202D',
+            '--light-grey': '#121721',
+            '--dark-blue-text': '#ffffff',
+            '--blue-opacity': 'rgba(255, 255, 255, .1)',
+            '--violet-button': '#ffffff',
+            '--blue-opacity-hover': 'rgba(255, 255, 255, .35)',
+            '--checkbox-unchecked': 'rgba(255, 255, 255, .135)'
+        }
+    };
+
+    useEffect(() => {
+        const newTheme = toggled ? themes.dark : themes.light;
+
+        for (let key in newTheme) {
+            document.documentElement.style.setProperty(key, newTheme[key]);
+        }
+    }, [toggled]);
 
     return (
         <div className='slider-container'>
